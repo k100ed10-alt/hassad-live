abc02b31200df03bf85a59096c805e97ef709bd3
k100ed10-alt <k100ed10@gmail.com>
2026-09-17 21:27:58 +0400

    Add Firebase config placeholder for Hassad

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+window.HASSAD_FIREBASE = {
+  enabled: false,
+  config: {
+    apiKey: "PASTE_API_KEY",
+    authDomain: "PASTE_PROJECT.firebaseapp.com",
+    projectId: "PASTE_PROJECT_ID",
+    storageBucket: "PASTE_PROJECT.appspot.com",
+    messagingSenderId: "PASTE_SENDER_ID",
+    appId: "PASTE_APP_ID"
+  }
+};
